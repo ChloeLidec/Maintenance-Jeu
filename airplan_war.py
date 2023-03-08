6185d0c12a466cb058f9f488b6667da6fc171885
Dikawu <chloe.lidec@etu.univ-orleans.fr>
2023-03-08 23:39:50 +0100

    #3 - Menu apparait et utilisable

--- airplan_war.py
+++ airplan_war.py
@@ -11,6 +11,8 @@ import pygame.locals as lo
 import constantes as const
 import player as pl
 import math
+import pygame_menu
+from pygame_menu.examples import create_example_window
 # Initialize the game
 pygame.init()
 screen = pygame.display.set_mode((const.SCREEN_WIDTH, const.SCREEN_HEIGHT))
@@ -19,6 +21,8 @@ pygame.display. set_caption('Airplane Wars')
 background = pygame.image.load('resources/image/background.png').convert()
 scroll = 0
 tiles = math.ceil(const.SCREEN_HEIGHT / background.get_height()) + 1
+
+difficulte = 0
 
 # Load the picture of the plane
 plane_img = pygame.image.load('resources/image/shoot.png')
@@ -164,89 +168,153 @@ def epaves(ennemies_down):
         enemy_down.down_index += 1
     return score_act
 
-clock = pygame.time.Clock()
-running = True
-score=0
-while running:
-    # Contrôlez la fréquence d'images maximale du jeu
-    clock.tick(45)
-    # Draw the background
-    i = 0
-    while i <tiles :
-        screen.blit(background, (0,background.get_height() * i + scroll))
-        i += 1
-    scroll -= 1
-    if abs(scroll) == background.get_height():
-        scroll = 0
-    # Draw an airplane
-    screen.blit(player.image[player.img_index], player.rect)
+def change_difficulty(values):
+    """Change la difficulté du jeu
 
-    # Gérer les événements
-    cpt_apparition_bullet = tirs(cpt_apparition_bullet,player)
-    deplacer_puces(player.bullets,player)
-    player.bullets.draw(screen)
+    Arguments:
+        values {tuple} -- Valeur de la difficulté"""
+    nom,index = values
+    difficulte = const.DIFFICULTY[index]
 
-    cpt_apparition_enemy=faire_apparaitre_ennemies(enemies1, cpt_apparition_enemy)
-    deplacer_ennemies(enemies1)
-    ennemies_touche(enemies1,player)
+def fonction_jeu():
+    global main_menu
+    clock = pygame.time.Clock()
+    running = True
+    score=0
 
-    # Dessinez l'animation de l'épave
-    score+=epaves(enemies_down)
-    enemies1.draw(screen)
-    # dessiner le score
-    score_font = pygame.font.Font(None, 36)
-    score_text = score_font.render(str(score), True, (128, 128, 128))
-    text_rect = score_text.get_rect()
-    text_rect.topleft = [10, 10]
-    screen.blit(score_text, text_rect)
-
-    # dessiner le score
-    score_font = pygame.font.Font(None, 36)
-    score_text = score_font.render(str(score), True, (128, 128, 128))
-    text_rect = score_text.get_rect()
-    text_rect.topleft = [10, 10]
-    screen.blit(score_text, text_rect)
-
-    # dessiner l'avion du joueur
-    if not player.is_hit:
+    main_menu.disable()
+    main_menu.full_reset()
+    while running:
+        # Contrôlez la fréquence d'images maximale du jeu
+        clock.tick(45)
+        # Draw the background
+        i = 0
+        while i <tiles :
+            screen.blit(background, (0,background.get_height() * i + scroll))
+            i += 1
+        scroll -= 1
+        if abs(scroll) == background.get_height():
+            scroll = 0
+        # Draw an airplane
         screen.blit(player.image[player.img_index], player.rect)
-        # Changer l'index de l'image pour animer l'avion
-        player.img_index = cpt_apparition_bullet // const.INDEX_ANIM
-    else:
-        player.img_index = player_down_index // const.INDEX_ANIM
-        screen.blit(player.image[player.img_index], player.rect)
-        player_down_index += 1
-        if player_down_index > 25:#gere le temps de l'animation de l'avion
-            running = False
 
-    # Update the screen
-    pygame.display.update()
-    key_pressed = pygame.key.get_pressed()
-    if key_pressed[lo.K_w] or key_pressed[lo.K_UP]:
-        player.move_up()
-    if key_pressed[lo.K_s] or key_pressed[lo.K_DOWN]:
-        player.move_down()
-    if key_pressed[lo.K_a] or key_pressed[lo.K_LEFT]:
-        player.move_left()
-    if key_pressed[lo.K_d] or key_pressed[lo.K_RIGHT]:
-        player.move_right()
+        # Gérer les événements
+        cpt_apparition_bullet = tirs(cpt_apparition_bullet,player)
+        deplacer_puces(player.bullets,player)
+        player.bullets.draw(screen)
 
-    # Process game exits
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+        cpt_apparition_enemy=faire_apparaitre_ennemies(enemies1, cpt_apparition_enemy)
+        deplacer_ennemies(enemies1)
+        ennemies_touche(enemies1,player)
 
-font = pygame.font.Font(None, 48)
-text = font.render('Score: '+ str(score), True, (255, 0, 0))
-text_rect = text.get_rect()
-text_rect.centerx = screen.get_rect().centerx
-text_rect.centery = screen.get_rect().centery + 24
-screen.blit(game_over, (0, 0))
-screen.blit(text, text_rect)
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-    pygame.display.update()
+        # Dessinez l'animation de l'épave
+        score+=epaves(enemies_down)
+        enemies1.draw(screen)
+        # dessiner le score
+        score_font = pygame.font.Font(None, 36)
+        score_text = score_font.render(str(score), True, (128, 128, 128))
+        text_rect = score_text.get_rect()
+        text_rect.topleft = [10, 10]
+        screen.blit(score_text, text_rect)
+
+        # dessiner le score
+        score_font = pygame.font.Font(None, 36)
+        score_text = score_font.render(str(score), True, (128, 128, 128))
+        text_rect = score_text.get_rect()
+        text_rect.topleft = [10, 10]
+        screen.blit(score_text, text_rect)
+
+        # dessiner l'avion du joueur
+        if not player.is_hit:
+            screen.blit(player.image[player.img_index], player.rect)
+            # Changer l'index de l'image pour animer l'avion
+            player.img_index = cpt_apparition_bullet // const.INDEX_ANIM
+        else:
+            player.img_index = player_down_index // const.INDEX_ANIM
+            screen.blit(player.image[player.img_index], player.rect)
+            player_down_index += 1
+            if player_down_index > 25:#gere le temps de l'animation de l'avion
+                running = False
+
+        # Update the screen
+        pygame.display.update()
+        key_pressed = pygame.key.get_pressed()
+        if key_pressed[lo.K_w] or key_pressed[lo.K_UP]:
+            player.move_up()
+        if key_pressed[lo.K_s] or key_pressed[lo.K_DOWN]:
+            player.move_down()
+        if key_pressed[lo.K_a] or key_pressed[lo.K_LEFT]:
+            player.move_left()
+        if key_pressed[lo.K_d] or key_pressed[lo.K_RIGHT]:
+            player.move_right()
+
+        # Process game exits
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+    font = pygame.font.Font(None, 48)
+    text = font.render('Score: '+ str(score), True, (255, 0, 0))
+    text_rect = text.get_rect()
+    text_rect.centerx = screen.get_rect().centerx
+    text_rect.centery = screen.get_rect().centery + 24
+    screen.blit(game_over, (0, 0))
+    screen.blit(text, text_rect)
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+        pygame.display.update()
+
+def main():
+    global main_menu
+    global surface
+
+    surface = create_example_window("Airplane war", (const.SCREEN_WIDTH, const.SCREEN_HEIGHT))
+    play_menu = pygame_menu.Menu(
+        height = const.SCREEN_HEIGHT,
+        title = 'Launch a game',
+        width = const.SCREEN_WIDTH)
+    play_menu.add.button('Play', fonction_jeu, difficulte)
+    play_menu.add.selector('Difficulty :', [('Easy', 0), ('Medium', 1),('Hard',2)], onchange=change_difficulty, selector_id='diff')
+    play_menu.add.button('Return to main menu', pygame_menu.events.BACK)
+
+    commands_theme = pygame_menu.themes.THEME_DARK.copy()
+    commands_menu = pygame_menu.Menu(
+        height = const.SCREEN_HEIGHT,
+        theme = commands_theme,
+        title = 'Commands',
+        width = const.SCREEN_WIDTH
+    )
+    commands_menu.add.label('Move : ↑ ← ↓ →')
+    commands_menu.add.label('Shoot : Space')
+    commands_menu.add.button('Return to main menu', pygame_menu.events.BACK)
+
+    main_menu = pygame_menu.Menu(
+        height = const.SCREEN_HEIGHT,
+        title = 'Airplane War',
+        width = const.SCREEN_WIDTH
+    )
+    main_menu.add.button('Play', play_menu)
+    main_menu.add.button('Commands', commands_menu)
+    main_menu.add.button('Quit', pygame_menu.events.EXIT)
+
+    while True:
+        #paint bg
+        screen.blit(background, (0, 0))
+
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                exit()
+        if main_menu.is_enabled():
+            main_menu.update(events)
+            main_menu.draw(surface)
+        
+        pygame.display.update()
+
+if __name__ == '__main__':
+    main()
+    
